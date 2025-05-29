7b29a8820204e487b7d71c4a0478b60eb803bda3
Maciej <147936508+mmaj1@users.noreply.github.com>
2025-05-29 08:51:16 +0200

    Update testVisitor.py

--- testVisitor.py
+++ testVisitor.py
@@ -3,17 +3,13 @@ from grammar.GraphgifLexer import GraphgifLexer
 from grammar.GraphgifParser import GraphgifParser
 from parsing.grammar.GraphgifVisitorImpl import GraphgifVisitorImpl
 
-# Wczytaj plik jako strumień
 input_stream = FileStream('./examples/example1.gg')
 
-# Lexer i TokenStream
 lexer = GraphgifLexer(input_stream)
 tokens = CommonTokenStream(lexer)
 
-# Parser
 parser = GraphgifParser(tokens)
 tree = parser.program()  # To jest parse tree!
 
-# Teraz visitor z ANTLR na parse tree
 visitor = GraphgifVisitorImpl()
 visitor.visit(tree)
